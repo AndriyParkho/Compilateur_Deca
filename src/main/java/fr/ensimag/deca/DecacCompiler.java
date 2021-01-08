8643f797ffbdcf0e9bd7c54281f82c42807d5a84
Etienne <etienne@DESKTOP-BG5AN0B.localdomain>
2021-01-08 13:54:50 +0100

    Ajout commentaire pour situer où se trouvera l'enrichissement de l'arbre (étape B)

--- src/main/java/fr/ensimag/deca/DecacCompiler.java
+++ src/main/java/fr/ensimag/deca/DecacCompiler.java
@@ -187,6 +187,7 @@ public class DecacCompiler {
 
 
         prog.verifyProgram(this);
+        //Ajouter l'enrichissement de l'arbre ici
         assert(prog.checkAllDecorations());
 
         addComment("start main program");
