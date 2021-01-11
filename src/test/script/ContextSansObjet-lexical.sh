f209dd18126acc02d236a1d1627bb2523097c73e
Etienne <etienne@DESKTOP-BG5AN0B.localdomain>
2021-01-11 22:03:49 +0100

    On peut maintenant choisir entre faire les tests valides, les tests invalides ou les deux

--- src/test/script/ContextSansObjet-lexical.sh
+++ src/test/script/ContextSansObjet-lexical.sh
@@ -9,13 +9,54 @@ cd "$(dirname "$0")"/../../.. || exit 1
 
 PATH=./src/test/script/launchers:"$PATH"
 
+echo "Tester les .deca [V] valid, [I] invalid ou [2] les deux ? :"
 
-for test_courant in ./src/test/deca/syntax/valid/provided/*.deca; do
-	if test_synt ./src/test/deca/syntax/valid/provided/hello.deca | grep -q -e 'Exception'
-	then
-		echo "\e[31mEchec inattendu de $test_courant"
-		exit 1
-	else
-		echo "\e[32mSucces attendu de $test_courant"
-	fi
-done
+read mode
+
+if [ $mode = "V" ]
+then
+	for test_courant in ./src/test/deca/syntax/valid/provided/*.deca; do
+		if test_lex $test_courant 2>&1 | grep -q -e $test_courant
+		then
+			echo "\e[31mEchec inattendu de $test_courant"
+			exit 1
+		else
+			echo "\e[32mSucces attendu de $test_courant"
+		fi
+	done
+elif [ $mode = "I" ]
+then
+	for test_courant in ./src/test/deca/syntax/invalid/provided/*.deca; do
+		if test_lex $test_courant 2>&1 | grep -q -e $test_courant
+		then
+			echo "\e[32mEchec attendu de $test_courant"
+		else
+			echo "\e[31mSucces inattendu de $test_courant"
+			exit 1
+		fi
+	done
+elif [ $mode = "2" ]
+then
+	echo "\e[37mvalid"
+	for test_courant in ./src/test/deca/syntax/valid/provided/*.deca; do 
+		if test_lex $test_courant 2>&1 | grep -q -e $test_courant
+		then
+			echo "\e[31mEchec inattendu de $test_courant"
+			exit 1
+		else
+			echo "\e[32mSucces attendu de $test_courant"
+		fi
+	done
+	echo "\e[37minvalid"
+	for test_courant in ./src/test/deca/syntax/invalid/provided/*.deca; do
+		if test_lex $test_courant 2>&1 | grep -q -e $test_courant
+		then
+			echo "\e[32mEchec attendu de $test_courant"
+		else
+			echo "\e[31mSucces inattendu de $test_courant"
+			exit 1
+		fi
+	done
+else
+	echo "Arguments acceptes : V, I ou 2"
+fi
