4a5223d513ddecda4cac551b1e6074af570bda4a
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2021-01-15 17:16:21 +0100

    Ajout de test et correction d'erreur dans un test

--- src/test/java/fr/ensimag/deca/context/TestPlusPlain.java
+++ src/test/java/fr/ensimag/deca/context/TestPlusPlain.java
@@ -25,14 +25,14 @@ public class TestPlusPlain {
     public void testType() throws ContextualError {
         DecacCompiler compiler = new DecacCompiler(null, null);
         AbstractExpr left = Mockito.mock(AbstractExpr.class);
-        when(left.verifyExpr(compiler, null, null)).thenReturn(INT);
+        when(left.verifyExpr(compiler, null)).thenReturn(INT);
         AbstractExpr right = Mockito.mock(AbstractExpr.class);
-        when(right.verifyExpr(compiler, null, null)).thenReturn(INT);
+        when(right.verifyExpr(compiler, null)).thenReturn(INT);
         Plus t = new Plus(left, right);
         // check the result
-        assertTrue(t.verifyExpr(compiler, null, null).isInt());
+        assertTrue(t.verifyExpr(compiler, null).isInt());
         // check that the mocks have been called properly.
-        verify(left).verifyExpr(compiler, null, null);
-        verify(right).verifyExpr(compiler, null, null);
+        verify(left).verifyExpr(compiler, null);
+        verify(right).verifyExpr(compiler, null);
     }
 }
