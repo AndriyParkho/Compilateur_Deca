d5814b5144d3d2cbb7ddf7e96130d16ac4fcd26e
Mohamed-Ali Lagha <mohamed-ali.lagha@phelma.grenoble-inp.fr>
2021-01-19 13:16:49 +0100

    ajout des classes manquantes

---
+++ src/main/java/fr/ensimag/deca/tree/Return.java
@@ -0,0 +1,49 @@
+package fr.ensimag.deca.tree;
+
+import java.io.PrintStream;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+
+public class Return extends AbstractInst{
+	
+	private AbstractExpr returnExpr;
+	public Return(AbstractExpr expr)
+	{
+		this.returnExpr=expr;
+	}
+	public AbstractExpr getReturnExpr()
+	{
+		return returnExpr;
+	}
+	@Override
+	protected void verifyInst(DecacCompiler compiler, ClassDefinition currentClass, Type returnType)
+			throws ContextualError {
+		// TODO 
+		
+	}
+	@Override
+	protected void codeGenInst(DecacCompiler compiler) throws jumpException {
+		// TODO 
+		
+	}
+	@Override
+	public void decompile(IndentPrintStream s) {
+		// TODO 
+		
+	}
+	@Override
+	protected void prettyPrintChildren(PrintStream s, String prefix) {
+		// TODO 
+		
+	}
+	@Override
+	protected void iterChildren(TreeFunction f) {
+		// TODO 
+		
+	}
+	
+}
