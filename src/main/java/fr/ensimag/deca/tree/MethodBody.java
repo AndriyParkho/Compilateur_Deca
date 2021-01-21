fab41fe7b133157091a55f280f2a627a342ae0fe
Etienne <etienne@DESKTOP-BG5AN0B.localdomain>
2021-01-21 18:59:49 +0100

    Ajout gestion d'un non return dans une méthode

--- src/main/java/fr/ensimag/deca/tree/MethodBody.java
+++ src/main/java/fr/ensimag/deca/tree/MethodBody.java
@@ -34,7 +34,19 @@ public class MethodBody extends AbstractMethodBody {
 		//FAIT
 		this.listDeclVar.verifyListDeclVariable(compiler, currentClass);
 		this.listInst.verifyListInst(compiler, currentClass, returnType);
-	    this.setType(returnType);
+		if (!returnType.isVoid()){
+			boolean isReturn = false;
+			for (AbstractInst inst : this.listInst.getList()){
+				isReturn = inst.getClass()==Return.class;
+			}
+			try{
+				if (!isReturn){
+					throw new ContextualError(String.format("La méthode doit renvoyer un %s", returnType.getName().getName()),
+							this.getLocation());
+				}
+			}catch(ContextualError ce){throw ce;}
+		}
+		this.setType(returnType);
 	}
 	
 	@Override
