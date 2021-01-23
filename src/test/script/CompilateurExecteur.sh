b4ecdc809fce2bbce12c9d9360209880424faefa
Etienne <etienne@DESKTOP-BG5AN0B.localdomain>
2021-01-23 16:35:05 +0100

    Changement description de l'exécutable

--- src/test/script/CompilateurExecteur.sh
+++ src/test/script/CompilateurExecteur.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Auteur : gl10
-# Version initiale : 16/01/2021
+# Version initiale : 23/01/2021
 
-#Programme bash qui lance decac et ima sur des tests .deca et vérifie que la sortie est identique à celle attendue dans le fichier .ans équvalent
+#Programme bash qui lance decac et ima un test entré en ligne de commande et affiche le résultat généré
 
 cd "$(dirname "$0")"/../../.. || exit 1
 
